ba19aac7c343c18d3d460b3fc5bbefbfefdd4db6
Steve Sephen <honguyentailoi05@gmail.com>
2024-05-30 19:04:36 +0700

    chỉnh sửa cơ bản
    tôi sửa chỗ này tạo bug chơi

--- gameover.cpp
+++ gameover.cpp
@@ -9,7 +9,7 @@ bool GameOver(SNAKE ){
     if (snake.a[0].x ==  || snake.a[0].x ==  || snake.a[0].y ==  || snake.a[0].y == )
         return true;
     return check_touch_snake(snake); 
-}
+}fgbdfbfddf nrdf
 bool snake(SNAKE& ){
     for(int i = 1; i <= .len; ++i){
         if(.a[i].x == .a[0].x && .a[i].y == .a[0].y)
