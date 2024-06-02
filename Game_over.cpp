9dfc87487a2ac2ec4afc6f23a75ce831a5c84e77
BeCaBuCode <23520950@gm.uit.com>
2024-06-02 22:08:56 +0700

    Update_GamOver

--- Game_over.cpp
+++ Game_over.cpp
@@ -1,10 +1,10 @@
-bool Game_Over(____ snake) //Cho nay de class con ran
+bool Game_Over(SNAKE snake)
 { 
     if (snake.a[0].x == 51 || snake.a[0].x == 151 || snake.a[0].y == 5 || snake.a[0].y == 35)
         return true;
     return check_touch_snake(snake); 
 }
-bool check_touch_snake(_____& snake){ //tuong tu
+bool check_touch_snake(SNAKE& snake){ 
     for(int i = 1; i <= snake.len; ++i){
         if(snake.a[i].x == snake.a[0].x && snake.a[i].y == snake.a[0].y)
             return true;
@@ -12,10 +12,10 @@ bool check_touch_snake(_____& snake){ //tuong tu
     return false;
 }
 void GAMEOVER(){
-    SetColor(6);
-    gotoxy(50,5);
-    cout << "#####   ###     #   #   #####  ##### ##    ## ##### ######"; 
-    gotoxy(50,6);
+    SetColor(7); //6
+    gotoxy(200,51); //50,5
+   cout << "#####   ###     #   #   #####  ##### ##    ## ##### ######"; 
+    gotoxy(200,52); //50,6
     cout << "## __  #   #   ### ###  ##__   #   #  ##  ##  ##___ ##__##";
     gotoxy(50,7);
     cout << "##  #  #####   #  #  #  ##     #   #   #  #   ##    ##  ##";
