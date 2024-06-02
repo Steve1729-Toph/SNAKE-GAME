a638c0cef74097b664ca71a8cae61638b6a128ca
Steve Sephen <honguyentailoi05@gmail.com>
2024-06-02 12:48:37 +0700

    Sửa code dựa trên chuẩn
    đã thay đổi và thêm hàm vẽ thức ăn theo tọa độ đã bàn bạc

--- Food.cpp
+++ Food.cpp
@@ -1,14 +1,21 @@
-bool touch(SNAKE& , int& , int&){
-    for(int i = 0; i < ?.len; ++i){
-        if( ?== a[i].x &&  == .a[i].y)
-            return true;        
+bool check_touch(SNAKE& snake, int& res_X, int& res_Y){
+    for(int i = 0; i < snake.len; ++i){
+        if(res_X == snake.a[i].x && res_Y == snake.a[i].y)
+            return true;
     }
     return false;
 }
-void Food(SNAKE& ,int& res_X, int& res_Y){
+void Create_Food(SNAKE& snake,int& res_X, int& res_Y){
     color = rand() % (15 - 1) + 1;
+    SetColor(color);
     do{
-        res_X = rand() % () + ;
-        res_Y = rand() % () + ;
-    }while(touch(snake, res_X, res_Y)); 
+        res_X = rand() % (150 - 52 + 1) + 52;
+        res_Y = rand() % (24 - 6 + 1) + 6;
+    }while(check_touch(snake, res_X, res_Y)); 
 }
+void Draw_Food(int& res_X, int& res_Y){
+    gotoxy(res_X, res_Y);
+    printf("%c", 254);
+} 
+
+//DA CHINH SUA DUA TREN HAM MAIN
